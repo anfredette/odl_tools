2d7e9986f49af32a60e92486318a535a950812bd
Andre Fredette <anfredette@gmail.com>
2016-08-01 16:41:13 -0400

    Change controller port # to default & rename create bridge script
    Signed-off-by: Andre Fredette <anfredette@gmail.com>

--- os_createbridges.sh
+++ os_createbridges.sh
@@ -3,6 +3,6 @@
 # sudo ovs-vsctl set-manager tcp:192.168.50.1:6640
 sudo ovs-vsctl add-br br-int
 sudo ovs-vsctl set bridge br-int protocols=OpenFlow13
-sudo ovs-vsctl set-controller br-int tcp:192.168.50.1:6633
+sudo ovs-vsctl set-controller br-int tcp:192.168.50.1:6653
 sudo ovs-vsctl add-br br-ex
 
